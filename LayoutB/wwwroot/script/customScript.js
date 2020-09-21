7323f9d4caff7109c49e9a74ba24ec2bdfe9594d
ade <ademolaogunkoya@gmail.com>
2020-09-21 14:11:22 +0100

    returned bootstrap files

---
+++ LayoutB/wwwroot/script/customScript.js
@@ -0,0 +1,33 @@
+
+
+
+
+
+
+
+
+
+function  ToggleSidebar()
+{
+    let hamParent_Obj=document.querySelector('hamburger_Parent');
+    let sidePanel_obj=document.querySelector('sidePanel');
+
+  let IsOff = sidePanel_obj.classList.contains('OFF');
+  let IsOn = sidePanel_obj.classList.contains('ON');
+
+ if(!IsOff && !IsOn)
+ {
+    sidePanel_obj.classList.add('ON');
+    
+ }
+
+ else if(IsOff)
+ {
+    sidePanel_obj.classList.replace('OFF','ON');
+ }
+
+ else
+ {
+    sidePanel_obj.classList.replace('ON','OFF');
+ }
+}
