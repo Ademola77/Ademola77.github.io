bf8cc7a59ba3ea24366e115be813042d7315fcae
ade <ademolaogunkoya@gmail.com>
2020-09-26 11:53:40 +0100

    Poi tx

--- LayoutB/wwwroot/script/customScript.js
+++ LayoutB/wwwroot/script/customScript.js
@@ -9,7 +9,7 @@ sidePanelID
 
 function  ToggleSidebar()
 {
-    let hamParent_Obj=document.querySelector('hamburger_Parent');
+    let hamParent_Obj=document.querySelector('.hamburger_Parent');
     //let sidePanel_obj=document.querySelector('sidePanel');
 
     let sidePanel_obj=document.getElementById('sidePanelID');
@@ -34,3 +34,18 @@ function  ToggleSidebar()
     sidePanel_obj.classList.replace('ON','OFF');
  }
 }
+
+
+
+
+const x=document.querySelector('.top');
+const x1=document.getElementById('top');
+x.addEventListener('mouseover',()=>{
+   x.style.background='red';
+   
+});
+x.addEventListener('mouseout',()=>{
+   x.style.background='blue';
+});
+
+//x.addEven)tListener(onclick)
